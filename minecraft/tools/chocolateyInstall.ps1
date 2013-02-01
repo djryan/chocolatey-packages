6ef3c6e8ac711c91bf4cbea942d74e2666bb578b
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-01 11:11:59 +0100

    bugfixing

--- minecraft/tools/chocolateyInstall.ps1
+++ minecraft/tools/chocolateyInstall.ps1
@@ -16,7 +16,9 @@ $url64 = $url # 64bit URL here or just use the same as $url
 try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
   # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
   # downloader that the main helpers use to download items
-  #Get-ChocolateyWebFile "$packageName" 'DOWNLOAD_TO_FILE_FULL_PATH' "$url" "$url64"
+  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $nodePath = Join-Path $scriptPath 'minecraft.exe'
+  Get-ChocolateyWebFile "$packageName" "$nodePath" "$url" "$url64"
   # installer, will assert administrative rights - used by Install-ChocolateyPackage
   #Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" '_FULLFILEPATH_' -validExitCodes $validExitCodes
   # unzips a file to the specified location - auto overwrites existing content
@@ -24,10 +26,10 @@ try { #error handling is only necessary if you need to do anything in addition t
   # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
   #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
   # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+  Install-ChocolateyPath "$nodePath" 'Machine' # Machine will assert administrative rights
   # add specific files as shortcuts to the desktop
   #$target = Join-Path $MyInvocation.MyCommand.Definition "$($packageName).exe"
-  #Install-ChocolateyDesktopLink $target
+  Install-ChocolateyDesktopLink $nodePath
   
   #------- ADDITIONAL SETUP -------#
   # make sure to uncomment the error handling if you have additional setup to do
