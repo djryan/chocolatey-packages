076ff2f6a9106182b914b7881143d0dbcf96819d
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-03 22:46:39 +0100

    tested common install path for all xampp packages

--- xampp.tool/tools/chocolateyInstall.ps1
+++ xampp.tool/tools/chocolateyInstall.ps1
@@ -2,9 +2,25 @@
 
 $binRoot = "$env:systemdrive\"
 if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-$xmppPath = join-path $binRoot $('xampp')
+$installDir = join-path $binRoot $('xampp')
 
-$url = 'http://www.apachefriends.org/download.php?xampp-win32-1.8.1-VC9.zip'
+$url = 'http://www.apachefriends.org/download.php?xampp-win32-1.8.1-VC9.7z'
 $url64 = $url
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$xmppPath" "$url64"
+try { 
+  $tempDir = "$env:TEMP\chocolatey\$($packageName)"
+  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+  $file = Join-Path $tempDir "$($packageName).7z"
+  Get-ChocolateyWebFile "$packageName" "$file" "$url"
+
+  # the 7zip file contains the folder xampp so the target folder has to be binRoot directly
+  Start-Process "7za" -ArgumentList "x -o`"$binRoot`" -y `"$file`"" -Wait
+  
+  if (![System.IO.Directory]::Exists($installDir)) { Write-ChocolateyFailure "$packageName" "xampp folder was not at the expectec position at $installDir" }
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
