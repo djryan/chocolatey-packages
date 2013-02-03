e87741bb58aa628b7886183813a7ccfe7dc3f7c4
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-03 16:03:30 +0100

    fix install dir for xamp
    installed/extracted always to systemdrive/xampp or
    chocolatey_bin_root/xampp

--- xampp.app/tools/chocolateyInstall.ps1
+++ xampp.app/tools/chocolateyInstall.ps1
@@ -1,8 +1,13 @@
 ﻿$packageName = 'XAMPP.app'
+
+$binRoot = "$env:systemdrive\"
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+$xmppPath = join-path $binRoot 'xampp'
+
 $installerType = 'exe'
 $url = 'http://www.apachefriends.org/download.php?xampp-win32-1.8.1-VC9-installer.exe'
 $url64 = $url
-$silentArgs = '/S'
+$silentArgs = "/S /D=`"$xmppPath`""
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- xampp.tool/tools/chocolateyInstall.ps1
+++ xampp.tool/tools/chocolateyInstall.ps1
@@ -1,6 +1,10 @@
 ﻿$packageName = 'XAMPP.tool'
+
+$binRoot = "$env:systemdrive\"
+if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+$xmppPath = join-path $binRoot $('xampp')
+
 $url = 'http://www.apachefriends.org/download.php?xampp-win32-1.8.1-VC9.zip'
 $url64 = $url
 
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+Install-ChocolateyZipPackage "$packageName" "$url" "$xmppPath" "$url64"
