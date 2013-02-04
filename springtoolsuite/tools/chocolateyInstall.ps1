f5bcc43ad6da359e22c0c5aa3c0b7b5e91b53ad9
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-04 19:27:22 +0100

    moved back to springsource directly under the binroot

--- springtoolsuite/tools/chocolateyInstall.ps1
+++ springtoolsuite/tools/chocolateyInstall.ps1
@@ -5,8 +5,6 @@ $url = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-sui
 $url64 = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-suite-3.1.0.RELEASE-e4.2-win32-x86_64.zip' # 64bit URL here or just use the same as $url
 
 try { 
-  #$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
-  ### For BinRoot, use the following instead ###
   $binRoot = "$env:systemdrive\"
   ### Using an environment variable to to define the bin root until we implement configuration ###
   if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
@@ -15,8 +13,6 @@ try {
   #Write-Host "Adding `'$installDir`' to the path and the current shell path"
   #Install-ChocolateyPath "$installDir"
   #$env:Path = "$($env:Path);$installDir"
-  #Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
-  ### OR for 7z ###
   
   if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
 
@@ -26,7 +22,7 @@ try {
   $file = Join-Path $tempDir "$($packageName).zip"
   Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64"
 
-  Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$file`"" -Wait
+  Start-Process "7za" -ArgumentList "x -o`"$binRoot`" -y `"$file`"" -Wait
 
   Write-ChocolateySuccess "$packageName"
 } catch {
