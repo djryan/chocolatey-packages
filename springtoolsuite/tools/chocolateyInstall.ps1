bbca387e553284a49c34103df3488313866e14e8
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-07 23:34:52 +0100

    final version for sts package
    added vfabric to path
    added desktop shortcut to sts

--- springtoolsuite/tools/chocolateyInstall.ps1
+++ springtoolsuite/tools/chocolateyInstall.ps1
@@ -1,20 +1,16 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-$packageName = 'springtoolsuite' # arbitrary name for the package, used in messages
-$url = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-suite-3.1.0.RELEASE-e4.2-win32.zip' # download url
-$url64 = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-suite-3.1.0.RELEASE-e4.2-win32-x86_64.zip' # 64bit URL here or just use the same as $url
+﻿$packageName = 'springtoolsuite'
+$url = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-suite-3.1.0.RELEASE-e4.2-win32.zip'
+$url64 = 'http://dist.springsource.com/release/STS/3.1.0/dist/e4.2/spring-tool-suite-3.1.0.RELEASE-e4.2-win32-x86_64.zip'
 
 try { 
   $binRoot = "$env:systemdrive\"
-  ### Using an environment variable to to define the bin root until we implement configuration ###
   if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $installDir = Join-Path $binRoot "$packageName"
-
-  #Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  #Install-ChocolateyPath "$installDir"
-  #$env:Path = "$($env:Path);$installDir"
+  $installDir = Join-Path $binRoot "springsource"
+  $stsDir = Join-Path $installDir "sts-3.1.0.RELEASE"
+  $stsFile = Join-Path $stsDir "STS.exe"
+  $vFabricDir = Join-Path $installDir "vfabric-tc-server-developer-2.7.2.RELEASE"
   
-  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+  #if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
 
   $tempDir = "$env:TEMP\chocolatey\$($packageName)"
   if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
@@ -24,6 +20,11 @@ try {
 
   Start-Process "7za" -ArgumentList "x -o`"$binRoot`" -y `"$file`"" -Wait
 
+  #Install-ChocolateyPath "$stsDir"
+  Install-ChocolateyPath "$vFabricDir"
+
+  Install-ChocolateyDesktopLink $stsFile
+  
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
