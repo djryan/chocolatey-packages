e1449f4f7e197de607012561970f4756312895ba
Alexander Bätz <baetz.alexander@googlemail.com>
2013-02-16 00:04:27 +0100

    added upx working towards lol

---
+++ upx/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'UPX'
+$url = 'http://upx.sourceforge.net/download/upx308w.zip' # download url
+$url64 = $url
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
