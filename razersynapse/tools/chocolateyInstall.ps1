32e4bdc69571be9a0076fdf78113fcbbd87f633d
Alexander Bätz <baetz.alexander@googlemail.com>
2013-03-02 16:34:47 +0100

    raser synapse package done

---
+++ razersynapse/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'RazerSynapse'
+$installerType = 'exe'
+$url = 'http://www.razersupport.com/index.php?_m=downloads&_a=downloadfile&downloaditemid=861'
+$url64 = $url
+$silentArgs = '/S /v/qn'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
