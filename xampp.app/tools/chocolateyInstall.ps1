01953b84dd75b16fd1de1a41895ceeab69a35daf
hputman <h.putman@ieee.org>
2013-10-30 10:41:48 -0400

    Fixed the XAMPP install command line options
    They switched installers, so "/S /D=C:\xampp" must now be: "--mode unattended --prefix C:\xampp"

--- xampp.app/tools/chocolateyInstall.ps1
+++ xampp.app/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $xmppPath = join-path $binRoot 'xampp'
 $installerType = 'exe'
 $url = 'http://www.apachefriends.org/download.php?xampp-win32-1.8.1-VC9-installer.exe'
 $url64 = $url
-$silentArgs = "/S /D=`"$xmppPath`""
+$silentArgs = "--mode unattended --prefix `"$xmppPath`""
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
